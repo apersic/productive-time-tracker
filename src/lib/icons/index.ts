792ab417ea360ddf9d3789d2e28bdf963024fd89
Alen Peršić <megalen61@gmail.com>
2026-09-10 22:53:50 +0200

    [PTT-5] Add icons

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,3 +1,6 @@
+export { default as DeleteIcon } from "./delete-icon.tsx";
+export { default as EditIcon } from "./edit-icon.tsx";
 export { default as EyeIcon } from "./eye-icon.tsx";
 export { default as EyeOffIcon } from "./eye-off-icon.tsx";
 export { default as LogOutIcon } from "./log-out-icon.tsx";
+export { default as MoreIcon } from "./more-icon.tsx";
